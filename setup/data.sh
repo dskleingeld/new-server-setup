af54599449c59c1dcd1db2a38b3fc88df7c10a69
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2021-06-27 21:45:31 +0200

    fill template vars in deploy script

--- setup/data.sh
+++ setup/data.sh
@@ -4,6 +4,15 @@ set -e
 # This script only sets up the user for the data and data splitter service
 # it does not compile the executable nor move them in place
 
+DOMAIN=$1
+TOKEN=$2
+DEV_DOMAIN=$3
+DEV_TOKEN=$4
+[ -z "$DOMAIN" ] || read -r -p "domain for the stable server: " DOMAIN
+[ -z "$TOKEN" ] || read -r -p "telegram token for the stable server: " TOKEN
+[ -z "$DEV_DOMAIN" ] || read -r -p "domain for the dev server: " DEV_DOMAIN
+[ -z "$DEV_TOKEN" ] || read -r -p "telegram on which to deploy: " DEV_TOKEN
+
 # add user if not yet present
 sudo adduser data
 
@@ -33,6 +42,15 @@ sudo chmod +x $SPLITTER
 # setup and start service
 for service in data data_dev datasplitter; do
 	sudo cp ../config/$service.service /etc/systemd/system/$service.service
+done
+
+# set env variables
+sed -i "s/<DOMAIN>/$DOMAIN/g" /etc/systemd/system/data.service
+sed -i "s/<TOKEN>/$TOKEN/g" /etc/systemd/system/data.service
+sed -i "s/<DOMAIN>/$DEV_DOMAIN/g" /etc/systemd/system/dev_data.service
+sed -i "s/<TOKEN>/$DEV_TOKEN/g" /etc/systemd/system/dev_data.service
+
+for service in data data_dev datasplitter; do
 	sudo systemctl enable $service
 	sudo systemctl start $service
 done
--- setup/ha.sh
+++ setup/ha.sh
@@ -4,6 +4,17 @@ set -e
 # This script only sets up the user for the home automation service
 # it does not compile the executable
 
+DOMAIN=$1
+TOKEN=$2
+ADMIN_PASS=$3
+HA_TOKEN=$4
+ALLOWED_IDS="${@:5}"
+[ -z "$DOMAIN" ] || read -r -p "domain for the server: " DOMAIN
+[ -z "$TOKEN" ] || read -r -p "telegram token for the server: " TOKEN
+[ -z "$ADMIN_PASS" ] || read -r -p "admin password" ADMIN_PASS
+[ -z "$HA_TOKEN" ] || read -r -p "token on which data is recieved: " HA_TOKEN
+[ -z "$ALLOWED_IDS" ] || read -r -p "telegram ids that can access the bot: " ALLOWED_IDS
+
 # add user if not yet present
 sudo adduser ha
 
@@ -20,5 +31,13 @@ sudo chmod +x /home/ha/homeAutomation
 
 # setup service
 cp ../config/ha.service /etc/systemd/system/ha.service
+
+# set templated vars
+sed -i "s/<DOMAIN>/$DOMAIN/g" /etc/systemd/system/ha.service
+sed -i "s/<TOKEN>/$TOKEN/g" /etc/systemd/system/ha.service
+sed -i "s/<ADMIN_PASS>/$ADMIN_PASS/g" /etc/systemd/system/ha.service
+sed -i "s/<HA_TOKEN>/$HA_TOKEN/g" /etc/systemd/system/ha.service
+sed -i "s/<ALLOWED_IDS>/$ALLOWED_IDS/g" /etc/systemd/system/ha.service
+
 sudo systemctl enable ha
 sudo systemctl start ha
