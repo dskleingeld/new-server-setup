93c1a75646edcec73bfde3a9dbfe20142146fde6
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2021-07-02 01:01:41 +0200

    fixes matrix setup script

--- setup/matrix.sh
+++ setup/matrix.sh
@@ -3,7 +3,6 @@
 # add user with home dir if not yet present
 id -u matrix&>/dev/null || sudo useradd -m matrix
 
-# EXEC=/home/matrix/conduit
 # TEXT=$(echo "please move: 
 # 	the matrix homeserver executable to $EXEC,
 # 	[press enter to continue]")
@@ -15,6 +14,7 @@ id -u matrix&>/dev/null || sudo useradd -m matrix
 # 	break
 # done
 
+EXEC=/home/matrix/conduit
 URL=https://conduit.rs/master/armv8/conduit-bin
 if ! sudo test -x $EXEC; then 
 	sudo wget -O $EXEC $URL
@@ -22,19 +22,16 @@ if ! sudo test -x $EXEC; then
 	sudo chown matrix:matrix $EXEC
 fi
 
-sudo chown -R data:data /home/data/
-sudo chmod +x $SERVER
-
-sudo cp ../config/matrix.service /etc/systemd/system/
+# set up config
 sudo cp ../config/conduit.toml /home/matrix/
 
 # set templated vars
 DOMAIN=$1
-[ -n "$DOMAIN" ] || read -r -p "name for matrix home server: " DOMAIN
-sudo sed -i "s/<DOMAIN>/$DOMAIN/g" /etc/systemd/system/matrix.service
-
-# setup and start service
+[ -n "$DOMAIN" ] || read -r -p "enter domain: " DOMAIN
+sudo sed -i "s/<DOMAIN>/$DOMAIN/g" /home/matrix/conduit.toml
 sudo chown matrix:matrix /home/matrix/conduit.toml
 
+# setup and start service
+sudo cp ../config/matrix.service /etc/systemd/system/
 sudo systemctl enable matrix
 sudo systemctl restart matrix
