7c44043ca58769eef89cd847bae88e427ca489d5
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2021-07-01 15:45:07 +0200

    improves request cert to deal with subdomains

--- bin/request_certs.sh
+++ bin/request_certs.sh
@@ -2,27 +2,36 @@
 set -e
 
 # updates ssl certificate and combines output into a single key understandable by HAproxy
-# must be run as root
+# must be run as root with arguments --renew or --request and second argument --production
+# to get valid requests (by default run agains letsencrypt staging env)
 
-DOMAIN="<DOMAIN>"
+DOMAIN="davidsk.dev"
+SUBDOMAINS=(data dev.data ha)
 PORT=34320  # local port to which 80 is forwarded
 EMAIL="admin@$DOMAIN"
 SOURCE="/etc/letsencrypt/live/$DOMAIN"
 TARGET_DIR="/etc/ssl/certs"
 CERT="$DOMAIN.pem"
 
+# build comma seperated list [@]/%/text adds a suffix
+# to each element in the SUBDOMAINS array
+domains="$DOMAIN ${SUBDOMAINS[@]/%/.$DOMAIN}"
+domains=$(echo $domains | tr " " ,)
+echo domains: $domains
+
 secs_since_change() {
 	curtime=$(date +%s)
-	filetime=$(stat $0 -C %Y)
+	filetime=$(stat $0 -c %Y)  # last modification, seconds since Epoch 
 	timediff=$(expr $curtime - $filetime)
 	echo $timediff
 }
 
-
+STAGING="--staging"
+[[ $2 == "--production" ]] && STAGING=""
 if [[ $1 == "--renew" ]]; then
-	certbot renew --http-01-port=$PORT
+	certbot renew $STAGING --http-01-port=$PORT
 elif [[ $1 == "--request" ]]; then
-	certbot certonly --standalone -d $DOMAIN \
+	certbot certonly $STAGING --standalone -d $domains \
 		--non-interactive --agree-tos --email $EMAIL \
 		--http-01-port=$PORT
 else
@@ -42,7 +51,7 @@ if [[ -z "$fullchain" ]]; then
 fi
 
 # exit if cert not renewed
-if [[ $(secs_since_change $fullchain) -gt 5 ]]; then
+if [[ $(secs_since_change $fullchain) -gt 30 ]]; then
 	printf "\033[0;33mCert not renewed, exiting\n"
 	exit 0
 fi
@@ -57,4 +66,3 @@ chmod 700 $TARGET_DIR/$CERT
 # Reload HAProxy if renew
 echo "certificates updated"
 [[ $1 == "--renew" ]] && systemctl reload haproxy
-
