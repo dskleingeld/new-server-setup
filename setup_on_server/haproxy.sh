6e28f433afdac3b40c8363f30582e93494c2ecf7
dvdsk <noreply@davidsk.dev>
2024-09-21 14:33:06 +0200

    remove cert renewal from haproxy setup

--- setup_on_server/haproxy.sh
+++ setup_on_server/haproxy.sh
@@ -31,27 +31,11 @@ SYSTEMD_DIR=/etc/systemd/system/
 sudo cp ../config/haproxy.service $SYSTEMD_DIR
 sudo systemctl enable haproxy
 
-
-# setup timer to run certificates update script twice a day
-sudo cp ../bin/request_certs.sh /usr/bin/
-sudo sed -i "s/<DOMAIN>/$DOMAIN/g" /usr/bin/request_certs.sh  # set domain
-sudo chown root:root /usr/bin/request_certs.sh  # no reason for mortals to access this
-
-sudo cp ../config/renew_certs.service $SYSTEMD_DIR
-sudo cp ../config/renew_certs.timer $SYSTEMD_DIR
-sudo systemctl enable renew_certs
-
-
 # ask user if they want to run initial certbot run
 if [[ ! -f /etc/ssl/certs/$DOMAIN.pem ]]; then
-	echo "need to request fresh certificate"
-
-	read -r -p "request fresh certificate? [y/N] " input
-	if [[ $input == "y" ]]; then
-		echo "requesting"
-		sudo /usr/bin/request_certs.sh --request
-	fi
+	echo "ERROR: need to set up certs (certs.sh) before haproxy can run"
+	echo "exiting"
+	exit
 fi
 
 sudo systemctl restart haproxy
-sudo systemctl restart renew_certs
